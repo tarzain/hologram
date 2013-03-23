05bdf52525ea5334b9199b710eb1675ea840c875
Zainul Shah <zan2434@gmail.com>
2013-03-23 13:11:42 -0400

    Quick fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
 	def base64_image file
 	  "<img src='data:image/jpeg;base64,#{file}' />".html_safe
 	end
+	def base64_image file, id
+	  "<img src='data:image/jpeg;base64,#{file}' />".html_safe
+	end
 end
